826cdf44be67bc0d54cd279255b16663bb773bbd
LindaTancini <lindatancini95@gmail.com>
2025-10-21 12:07:49 +0200

    feat: add card back

---
+++ src/components/Card/CardBack.tsx
@@ -0,0 +1,28 @@
+// Importazioni
+import "./Card.css";
+import { Button } from "../Button/Button";
+
+// Definizione del tipo delle proprietà accettate dal componente CardBack
+export type CardBackProps = {
+  title: string;
+  ingredients: string[];
+};
+
+// Definizione del componente CardBack come funzione React
+export function CardBack({ title, ingredients }: CardBackProps) {
+  return (
+    <article className="card-back" aria-label={`Ingredienti di ${title}`}>
+      <div className="card-content">
+        <h4 className="card-title">Ingredienti</h4>
+        <ul>
+          {ingredients.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="card-footer">
+        <Button label="Torna indietro" size="medium" />
+      </div>
+    </article>
+  );
+}
